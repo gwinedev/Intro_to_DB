1f6ad89872711a2829dc76e2802c368692a44fd6
Okoeguale Godwin <okoegualegodwin@gmail.com>
2025-10-18 13:08:09 +0100

    implement first task

---
+++ alx_book_store.sql
@@ -0,0 +1,37 @@
+-- Create Database
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+USE alx_book_store;
+
+-- Create authors' table
+CRETAE TABLE AUTHORS(
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
+);
+
+--Create customer's table
+CREATE TABLE CUSTOMERS(
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+)
+
+-- Create Book's table
+CREATE TABLE BOOKS(
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INT NOT NULL,
+    price DOUBLE,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES AUTHORS(author_id)
+)
+
+-- Create Order Details
+CREATE TABLE ORDER_DETAILS(
+    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT NOT NULL,
+    book_id INT NOT NULL,
+    quantity DOUBLE NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES ORDERS(order_id),
+    FOREIGN KEY (book_id) REFERENCES BOOKS(book_id)
+)
