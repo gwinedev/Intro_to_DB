f7855667fcbcad991a1db2e2aa186910c404d30d
Okoeguale Godwin <okoegualegodwin@gmail.com>
2025-10-18 13:10:34 +0100

    write authors in small letters

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
 -- Create authors' table
-CRETAE TABLE AUTHORS(
+CRETAE TABLE Authors(
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
